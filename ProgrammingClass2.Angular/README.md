743e4b9b965218b025200627c7f7dc9a0bbf260b
Hayk Shirinyan <haykshirinyan@gmail.com>
2021-10-17 11:55:55 -0700

    Added readme file.

---
+++ ProgrammingClass2.Angular/README.md
@@ -0,0 +1,6 @@
+﻿
+Crangirn arajin angam run aneluc, hetevyal banere petq e install anel. Sa petq e anel miayn arajin angam cragirn ashaxatacneluc.
+
+1. Download are u install https://nodejs.org/en/download/
+2. Bacum eq command line eve hetevyal hramann eq run anum
+	npm install -g @angular/cli
--- ProgrammingClass2.Angular/Startup.cs
+++ ProgrammingClass2.Angular/Startup.cs
@@ -90,11 +90,6 @@ namespace ProgrammingClass2.Angular
                 // see https://go.microsoft.com/fwlink/?linkid=864501
 
                 spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseAngularCliServer(npmScript: "start");
-                }
             });
         }
     }
