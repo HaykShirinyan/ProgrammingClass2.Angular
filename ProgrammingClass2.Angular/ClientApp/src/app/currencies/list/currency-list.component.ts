1eb41c4db076748d59be9bcaa4de72ae69560be9
André Arshakian <andrearshakian@gmail.com>
2021-10-30 15:39:37 +0200

    Added currency and productType list page

---
+++ ProgrammingClass2.Angular/ClientApp/src/app/currencies/list/currency-list.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { Currency } from "../../shared/models/currency";
+
+
+@Component({
+  templateUrl: './currency-list.component.html'
+})
+export class CurrencyListComponent implements OnInit {
+  private readonly _http: HttpClient;
+
+  public currencies: Currency[];
+
+  constructor(http: HttpClient) {
+    this._http = http;
+  }
+
+  public ngOnInit(): void {
+    this._http.get<Currency[]>('api/currencies').subscribe(apiCurrencies => {
+      this.currencies = apiCurrencies
+    });
+  }
+}
