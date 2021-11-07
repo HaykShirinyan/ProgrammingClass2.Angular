e9c87603f3a37b49d5f11183701051e31bba7cde
Hayk Shirinyan <haykshirinyan@gmail.com>
2021-11-07 12:25:29 -0800

    Added Unit of Measure to Products as foreign key.

--- ProgrammingClass2.Angular/ClientApp/src/app/products/create/create-product.component.ts
+++ ProgrammingClass2.Angular/ClientApp/src/app/products/create/create-product.component.ts
@@ -1,22 +1,35 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Product } from "../../shared/models/product";
+import { UnitOfMeasure } from "../../shared/models/unit-of-measure";
 import { ProductService } from "../../shared/services/product.service";
+import { UnitOfMeasureService } from "../../shared/services/unit-of-measure.service";
 
 @Component({
   templateUrl: './create-product.component.html'
 })
-export class CreateProductComponent {
+export class CreateProductComponent implements OnInit {
   private readonly _productService: ProductService;
   private readonly _router: Router;
+  private readonly _unitOfMeasureService: UnitOfMeasureService;
 
   // {} nshanakum e datark object.
   public product: Product = {};
+  public unitOfMeasures: UnitOfMeasure[] = [];
 
-  constructor(productService: ProductService, router: Router) {
+  constructor(
+    productService: ProductService,
+    router: Router,
+    unitOfMeasureService: UnitOfMeasureService
+  ) {
     this._productService = productService;
     this._router = router;
+    this._unitOfMeasureService = unitOfMeasureService;
+  }
+
+  public async ngOnInit(): Promise<void> {
+    this.unitOfMeasures = await this._unitOfMeasureService.getAll();
   }
 
   public async createProduct(form: NgForm): Promise<void> {
