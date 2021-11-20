c85bd9a3e75831b8a55e28710571a998730dab36
André Arshakian <andrearshakian@gmail.com>
2021-11-20 16:29:24 +0100

    More Corrections

--- ProgrammingClass2.Angular/Controllers/ProductsController.cs
+++ ProgrammingClass2.Angular/Controllers/ProductsController.cs
@@ -58,8 +58,8 @@ namespace ProgrammingClass2.Angular.Controllers
                 _context.Products.Add(product);
                 _context.SaveChanges();
 
-                    return Ok(product);
-                }
+                return Ok(product);
+            }
 
             // BadRequest(ModelState) het enq veradardznum erb ModelStat valid chi.
             // Aysinqn erb validaiton-i error-ner kan.
